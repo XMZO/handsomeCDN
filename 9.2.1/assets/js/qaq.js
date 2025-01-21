8aa2c1181c2a5587f8401410dab2fe1c7b34d659
XMZO <chenjunbo3166279407@outlook.com>
2025-01-22 02:21:03 +0800

    Update qaq.js

--- 9.2.1/assets/js/qaq.js
+++ 9.2.1/assets/js/qaq.js
@@ -1,16 +1,17 @@
 /*标题卖萌*/
-var OriginTitile = document.title,
-titleTime;
-document.addEventListener("visibilitychange",
-function() {
+var OriginTitle = document.title,
+    titleTime;
+document.addEventListener("visibilitychange", function() {
     if (document.hidden) {
-        document.title = "网页已崩溃！";
-        clearTimeout(titleTime)
-    } else {
-        document.title = "(/≧▽≦/)你又回来了！ " ;
         titleTime = setTimeout(function() {
-            document.title = OriginTitile
-        },
-        2000)
+            document.title = "(/ω＼)网页已崩溃！";
+        }, 1000);
+    } else {
+        // 清除之前的定时器
+        clearTimeout(titleTime);
+        document.title = "(/≧▽≦/)你又回来了！";
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
     }
 });
