22f51c229a7c076e150201281434eefda92a6453
XMZO <chenjunbo3166279407@outlook.com>
2025-01-22 02:29:21 +0800

    Update qaq.js

--- 9.2.1/assets/js/qaq.js
+++ 9.2.1/assets/js/qaq.js
@@ -4,12 +4,12 @@ var OriginTitle = document.title,
 document.addEventListener("visibilitychange", function() {
     if (document.hidden) {
         titleTime = setTimeout(function() {
-            document.title = "(/ω＼)网页已崩溃！";
+            document.title = "Σ( ° △ °|||)网页已崩溃!";
         }, 1000);
     } else {
         // 清除之前的定时器
         clearTimeout(titleTime);
-        document.title = "(/≧▽≦/)你又回来了！";
+        document.title = "(/≧▽≦/)你又回来了!";
         titleTime = setTimeout(function() {
             document.title = OriginTitle;
         }, 2000);
